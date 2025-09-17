b3a89700c4bab18a1eea4975e94fc47121745fdd
Nethmina-Official <nethminaofc@outlook.com>
2025-09-17 20:45:42 +0530

    Create trt.js

---
+++ plugins/trt.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const {cmd , commands} = require('../command');
+
+cmd({
+    pattern: "trt",
+    alias: ["translate"],
+    desc: "🌍 Translate text between languages",
+    react: "⚡",
+    category: "other",
+    filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+    try {
+        const args = q.split(' ');
+        if (args.length < 2) return reply("❗ Please provide a language code and text. Usage: .translate [language code] [text]");
+
+        const targetLang = args[0];
+        const textToTranslate = args.slice(1).join(' ');
+
+        const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(textToTranslate)}&langpair=en|${targetLang}`;
+
+        const response = await axios.get(url);
+        const translation = response.data.responseData.translatedText;
+
+        const translationMessage = `
+🌍 *NETHMINA-OFC-WABOT TRANSLATION* 🌍
+
+🔤 *Original*: ${textToTranslate}
+
+🔠 *Translated*: ${translation}
+
+🌐 *Language*: ${targetLang.toUpperCase()}
+
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪɴᴀ ᴏꜰᴄ ||`;
+
+        return reply(translationMessage);
+    } catch (e) {
+        console.log(e);
+        return reply("⚠️ An error occurred data while translating the your text. Please try again later🤕");
+    }
+});
